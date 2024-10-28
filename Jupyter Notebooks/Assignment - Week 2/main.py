bbcbb841a4d06625f3bf3ab0907504db041cfbaf
AmeetZ <ameetzdev@gmail.com>
2024-10-28 16:16:27 +0530

    Mid-Update of Week 2 Assignment

--- Jupyter Notebooks/Assignment - Week 2/main.py
+++ Jupyter Notebooks/Assignment - Week 2/main.py
@@ -1,1 +1,15 @@
 # Build a CNN for the Fashion MNIST Dataset and Benchmark it against the previous Neural Network which used the same dataset (Week 1 Assignment)
+
+from functions import * 
+import tensorflow as tf
+
+# Importing Fashion MNIST Dataset
+(train_image_data, train_image_label), (test_image_data, test_image_label) = tf.keras.datasets.fashion_mnist.load_data()
+
+# Checking the dataset shape
+print("\nTraining Image Data: ", train_image_data.shape)
+print("Training Image Labels: ", train_image_label.shape)
+print("\nTesting Image Data: ", test_image_data.shape)
+print("Testing Image Labels: ", test_image_label.shape)
+
+gradient_x, gradient_y, gradient_magnitude = apply_sobel_filter(train_image_data)
